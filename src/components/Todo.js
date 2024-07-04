0b05cf0c80dac1cc3dd72dbd78fc7046f0e1abc0
navya1220 <navyakalla1111@gmail.com>
2024-07-04 12:20:23 +0530

    Update React app with new changes

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,16 +1,21 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
-export const Todo = ({task, deleteTodo, editTodo, toggleComplete}) => {
+export const Todo = ({ task, deleteTodo, editTodo, toggleComplete }) => {
   return (
     <div className="Todo">
-        <p className={`${task.completed ? "completed" : "incompleted"}`} onClick={() => toggleComplete(task.id)}>{task.task}</p>
-        <div>
+      <p className={`${task.completed ? "completed" : "incompleted"}`} onClick={() => toggleComplete(task.id)}>
+        {task.task}
+      </p>
+      <div>
+        <button className={`task-button ${task.completed ? 'completeded' : 'circle'}`} onClick={() => toggleComplete(task.id)}>
+          {task.completed ? '✅' : '⬤'}
+        </button>
         <FontAwesomeIcon className="edit-icon" icon={faPenToSquare} onClick={() => editTodo(task.id)} />
         <FontAwesomeIcon className="delete-icon" icon={faTrash} onClick={() => deleteTodo(task.id)} />
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
